8ddf66c390c9056a9f641834dec17352195664aa
Joanna Świderska <joannaswiderska@MacBook-Pro-Joanna.local>
2022-07-12 00:37:55 +0200

    Add loader and views result

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const amount = document.querySelector(".amount"); //input
 const currency = document.querySelector("#currency"); //selector
 const swapBtn = document.querySelector(".swap");
+const appBody = document.querySelector(".app-body");
+// const loader = document.querySelector(".show-loader");
 const rateInfo = document.querySelector(".rate-info");
 
-const calculateCur = () => {
-  console.log(currency);
-  // endresult.textContent = "Calculating....";
+const addLoader = () => {
+  setTimeout(() => {
+    const loader = `<div class="loader"></div>`;
+    appBody.insertAdjacentHTML("beforeend", loader);
+  }, 1000);
+};
+
+// const loaderTimeout = setTimeout(addLoader, 1000);
+
+const calculateCur = (event) => {
+  event.preventDefault();
   axios
     .get(`http://api.nbp.pl/api/exchangerates/rates/A/${currency.value}/`)
     .then((res) => {
@@ -21,10 +31,17 @@ const calculate = (responses) => {
     alert("Wpisz kwotę");
   } else {
     const result = amount.value * responses;
-    rateInfo.textContent = result;
+    const viewResult = `<h3 class="result">${amount.value} ${
+      currency.value
+    } = ${result.toFixed(2)} ZŁ</h3>`;
+    rateInfo.insertAdjacentHTML("beforeend", viewResult);
+    const span = `<span class="info">1 ${currency.value} to ${responses} zł</span>`;
+    rateInfo.insertAdjacentHTML("beforeend", span);
   }
 };
 
 // amount.addEventListener("input", calculateCur);
 // currency.addEventListener("change", calculateCur);
-swapBtn.addEventListener("click", calculateCur);
+swapBtn.addEventListener("click", addLoader);
+clearTimeout(addLoader);
+swapBtn.addEventListener("click", (event) => calculateCur(event));
